cab3432d5a4ccde7fab9849970242c213ed54880
Charles Dowd <charlie@universitycredit.com>
2024-05-03 15:58:09 +0800

    remove welcome back toast

--- client/src/store/slices/authSlice.ts
+++ client/src/store/slices/authSlice.ts
@@ -56,7 +56,6 @@ const authSlice = createSlice({
       (state, { payload }) => {
         state.accessToken = payload.accessToken;
         state.user = payload.user;
-        toast.success('Welcome Back!');
       },
     );
 
